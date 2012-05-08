7aa6fd035fd49c1e62ebf7ed1eabf288dd849ebe
Aleksandar Gargenta <sasa@gargenta.com>
2012-05-08 01:34:50 -0700

    Re-formatted

--- src/com/marakana/android/lognative/LogLib.java
+++ src/com/marakana/android/lognative/LogLib.java
@@ -1,10 +1,9 @@
-
 package com.marakana.android.lognative;
 
 import android.util.Log;
 
 public class LogLib {
-    public static void logJ(int priority, String tag, String msg) {
-        Log.println(priority, tag, msg);
-    }
+	public static void logJ(int priority, String tag, String msg) {
+		Log.println(priority, tag, msg);
+	}
 }
