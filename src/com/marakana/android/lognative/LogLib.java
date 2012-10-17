86d8312731448e95866af112f0039a17ae6715e0
Aleksandar Gargenta <sasa@gargenta.com>
2012-10-17 10:42:05 -0700

    Better UI

--- src/com/marakana/android/lognative/LogLib.java
+++ src/com/marakana/android/lognative/LogLib.java
@@ -1,9 +1,14 @@
+
 package com.marakana.android.lognative;
 
 import android.util.Log;
 
 public class LogLib {
-	public static void logJ(int priority, String tag, String msg) {
-		Log.println(priority, tag, msg);
-	}
+    public static void logJ(int priority, String tag, String msg) {
+        Log.println(priority, tag, msg);
+    }
+
+    public static void logN(int priority, String tag, String msg) {
+        throw new UnsupportedOperationException("Implement me as a native method!");
+    }
 }
